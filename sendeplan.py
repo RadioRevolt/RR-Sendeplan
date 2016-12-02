d396f698d30fe1e3818b3e01416180b597ad6f04
Thorben Werner Sjøstrøm Dahl <thorben@sjostrom.no>
2016-12-02 23:31:56 +0100

    Fix Flask assuming application root is /

--- sendeplan.py
+++ sendeplan.py
@@ -1,10 +1,51 @@
 import datetime
 
 from flask import Flask, render_template, make_response, abort, redirect, url_for
+from werkzeug.contrib.fixers import ProxyFix
 import requests
 from yaml import load
 
 app = Flask(__name__)
+
+class ReverseProxied(object):
+    '''Wrap the application in this middleware and configure the 
+    front-end server to add these headers, to let you quietly bind 
+    this to a URL other than / and to an HTTP scheme that is 
+    different than what is used locally.
+
+    In nginx:
+    location /myprefix {
+        proxy_pass http://192.168.0.1:5001;
+        proxy_set_header Host $host;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Scheme $scheme;
+        proxy_set_header X-Script-Name /myprefix;
+        }
+
+    :param app: the WSGI application
+    '''
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
+        if script_name:
+            environ['SCRIPT_NAME'] = script_name
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+
+        server = environ.get('HTTP_X_FORWARDED_SERVER', '')
+        if server:
+            environ['HTTP_HOST'] = server
+        return self.app(environ, start_response)
+
+
+app.wsgi_app = ReverseProxied(app.wsgi_app)
 
 def load_configuration(configfile):
     with open(configfile, "rb") as fp:
